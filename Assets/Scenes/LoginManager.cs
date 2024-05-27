67626d2196b300ca919c34c5581b6332cea42403
solmerani <148452997+solmerani@users.noreply.github.com>
2024-05-27 09:06:52 -0300

    checkpassword

---
+++ Assets/Scenes/LoginManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField passwordinput;
+    private string contraseña = "hola";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (passwordinput.text == contraseña)
+        {
+            Debug.Log("Contraseña Correcta");
+        }
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void checkPassword()
+    {
+        if (passwordinput.text == contraseña)
+        {
+            Debug.Log("Contraseña Correcta");
+        }
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
